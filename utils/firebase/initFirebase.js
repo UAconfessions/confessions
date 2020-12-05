4adb7bbe1b7f3ea6e951f6c455090c52a8b109d6
sam@apostel.be <sam@apostel.be>
2020-12-05 15:29:30 +0100

    client side env vars should start with NEXT_PUBLIC_ don't listen to the previous commit, he's a liar

--- utils/firebase/initFirebase.js
+++ utils/firebase/initFirebase.js
@@ -2,10 +2,10 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_CLIENT_DB_URL,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_DB_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 }
 
 export default function initFirebase() {
