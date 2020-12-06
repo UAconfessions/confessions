a5b2b89cb594388dc8f7e69a86bc6a0408f8829c
Sam-Apostel <sam@apostel.be>
2020-12-07 00:12:21 +0100

    don't redefine a variable that was passed to a function. renamed id

--- utils/facebook/facebook.js
+++ utils/facebook/facebook.js
@@ -1,8 +1,8 @@
 export const postToFacebook = async ({value, id}) => {
 	try{
 		const message = `#${id} ${value}`;
-		const { id } =  await request('feed', {message});
-		return {facebook_post_id: id};
+		const answer = await request('feed', {message});
+		return {facebook_post_id: answer.id};
 	}catch(error){
 		console.error(error);
 		return {facebook_post_error: JSON.stringify(error)};
@@ -13,5 +13,9 @@ const request = async (endpoint, searchParams) => {
 	const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
 	const url = new URL(`v9.0/${process.env.FACEBOOK_PAGE_ID}/${endpoint}`, `https://graph.facebook.com`);
 	url.search = new URLSearchParams({...searchParams, access_token});
-	return await fetch(url,{method: 'POST'}).then(res => res.json());
+	return await fetch(url,{method: 'POST'}).then(res => {
+	console.log(res.json());
+	return res.json();
+	});
+	
 };
