fc46dd66515cd7091595a9f6ee1607de4b5ecba9
sam@apostel.be <sam@apostel.be>
2021-04-06 11:26:56 +0200

    remove voicemail

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -2,22 +2,13 @@ import {getPresignedUrl, verifyIdToken} from "../../../utils/firebase/firebase";
 
 module.exports = async ({query: { file, type }, headers: { token, 'x-real-ip': realIp }, connection}, res) => {
 	const ip = realIp || connection.remoteAddress;
-	if (type === 'voicemail'){
-		try {
-			const presignedUrl = await getPresignedUrl(file, `${type}`, ip);
-			res.status(200).json(presignedUrl);
-		}catch(e){
-			res.status(401).json({message: 'Unauthorized', code: 401});
-		}
-	}else if(type === 'pending'){
-		try {
-			const user = await verifyIdToken(token);
-			const presignedUrl = await getPresignedUrl(file, `${type}/${user.uid}`, user.email);
-			res.status(200).json(presignedUrl);
-		}catch(e){
-			res.status(401).json({message: 'Unauthorized', code: 401});
-		}
-	}else{
-		res.status(400).json({message: 'Bad request', code: 400});
+
+	try {
+		const user = await verifyIdToken(token);
+		const presignedUrl = await getPresignedUrl(file, `${type}/${user.uid}`, user.email);
+		res.status(200).json(presignedUrl);
+	}catch(e){
+		res.status(401).json({message: 'Unauthorized', code: 401});
 	}
+
 }
