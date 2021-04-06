0614d07d10aa7a1560c4703b11e6d125ee6ce3b9
sam@apostel.be <sam@apostel.be>
2021-04-06 11:30:00 +0200

    remove download and browsing files

--- pages/api/download/index.js
+++
@@ -1,9 +0,0 @@
-import { getDownloadableUrl } from "../../../utils/firebase/firebase";
-import request from 'request';
-
-module.exports = async ({query: { file: fileName, path }}, res) => {
-	const url = await getDownloadableUrl(fileName, path);
-	res.setHeader(`content-disposition`, `attachment; filename=${fileName}`);
-	request(url).pipe(res);
-	//res.status(200).json(url);
-}
