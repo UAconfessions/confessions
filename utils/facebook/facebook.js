365801cd916fd80d0d4c78f8c91906d01f3fe526
sam@apostel.be <sam@apostel.be>
2021-04-10 18:28:07 +0200

    fix photo upload instead of passing the url to facebook, we fetch the photo ourselves.
    might need to look into making confession posts asyncronous because this takes a while

--- utils/facebook/facebook.js
+++ utils/facebook/facebook.js
@@ -1,14 +1,19 @@
+import request from 'request';
+import FormData from 'form-data';
+
 export const postToFacebook = async ({value, id, url}) => {
 	try{
 		const message = `#${id} ${value}`;
 
 		if (url){
 			const res = await postPhoto(message, url);
-			console.log({res, url});
-			return {facebook_post_id: undefined};
+			if (res.id) return {facebook_post_id: res.post_id};
+			else throw res;
+		}else{
+			const res = await postText(message);
+			if (res.id) return {facebook_post_id: res.id};
+			else throw res;
 		}
-		const { id: facebook_post_id } = await postText(message);
-		return {facebook_post_id};
 
 	}catch(error){
 		console.error(error);
@@ -16,14 +21,23 @@ export const postToFacebook = async ({value, id, url}) => {
 	}
 };
 
-export const postText = (message) => request('feed', {message});
+export const postText = (message) => execute('feed', {message});
 
-export const postPhoto = (caption, url) => request('photos', { url, caption, published: true });
+export const postPhoto = async (caption, url) => {
+	const file = await request(url);
+	const imageData = new FormData();
+	imageData.append('source', file);
 
-const request = async (endpoint, searchParams) => {
+	return execute('photos', { caption, published: true }, imageData);
+}
+
+const execute = async (endpoint, searchParams, body) => {
 	const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
 	const url = new URL(`v10.0/${process.env.FACEBOOK_PAGE_ID}/${endpoint}`, `https://graph.facebook.com`);
 	url.search = new URLSearchParams({...searchParams, access_token});
-	return await fetch(url,{method: 'POST'}).then(res => res.json());
 
+	const params = {method: 'POST'};
+	if(body) params.body = body;
+
+	return await fetch(url, params).then(res => res.json());
 };
